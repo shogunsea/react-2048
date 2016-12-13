0484dadf01cff934a4570d04aaf6372d363c6a57
shogunsea <shogunsea08@gmail.com>
2016-12-12 19:56:29 -0600

    chalk for logging; placeholder for promisify async tasks

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,8 +3,10 @@
 const gulp = require('gulp');
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 const exists = fs.existsSync;
 const rootPath = path.resolve('.');
 const deployKeyPath = path.join(rootPath, './.deploykey.json');
@@ -13,10 +15,18 @@ if (!exists(deployKeyPath)) {
     return;
 }
 
+const currentSHA = execSync('git rev-parse HEAD');
+// const SHAFilePath = path.join(rootPath,, './SHA.txt');
+// const readCurrentLocalSHA = new Promise();
+// const updateSHAPromise = new Promise();
+// const deployWithCurrentSHA = new Promise();
+
 const deployKey = require('../.deploykey.json');
 let command = `rsync -avzP --exclude='.*' --exclude='node_modules' --exclude='*.txt' --exclude='tmp' . ${deployKey.path}:~/2048`
 
 gulp.task('deploy', 'deploy to production host',  function() {
+  console.log(chalk.green('deploy current SHA: ') + chalk.red(currentSHA) + chalk.green(' to production host...'));
+  // readCurrentLocalSHA.then(updateSHAPromise).then(deployWithCurrentSHA).catch().doSomethingElse();
   exec(command, function(err, stdout, stderr) {
     console.log(stdout);
     console.log(command);
