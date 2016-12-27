9ff3735b2f64db5dc112232a7490cbd22bc41a35
shogunsea <shogunsea08@gmail.com>
2016-12-26 20:37:56 -0600

    promise error handling

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -7,14 +7,9 @@ const chalk = require('chalk');
 const exec = require('child_process').exec;
 
 const rootPath = path.resolve('.');
-const deployKeyPath = path.join(rootPath, './.deploykey.json');
 const exists = fs.existsSync;
 
 const SHAFilePath = path.join(rootPath, './SHA.txt');
-const deployKey = require('../.deploykey.json');
-const deployCommand = `rsync -avzP --exclude='.*'
-  --exclude='node_modules' --exclude='*.txt'
-  --exclude='tmp' . ${deployKey.path}:~/2048`;
 
 const readPreviousSavedSHA = function() {
   return new Promise(function(resolve, reject) {
@@ -50,7 +45,7 @@ const updateSavedSHA = ({currentSHA, previousSHA}) => {
   });
 };
 
-const deployToProduction = ({currentSHA, previousSHA}) => {
+const deployToProduction = ({currentSHA, previousSHA, deployCommand}) => {
   return new Promise((resolve, reject) => {
     exec(deployCommand, function(err, stdout, stderr) {
       if (err)
@@ -62,15 +57,21 @@ const deployToProduction = ({currentSHA, previousSHA}) => {
 };
 
 gulp.task('deploy', 'Deploy to production host', () => {
+  const deployKeyPath = path.join(rootPath, './.deploykey.json');
+
   if (!exists(deployKeyPath)) {
     return;
   }
+
+  const deployKey = require('../.deploykey.json');
+  const deployCommand = `rsync -avzP --exclude='.*' \
+  --exclude='node_modules' --exclude='*.txt' \
+  --exclude='tmp' . ${deployKey.path}:~/2048`;
 
   let diffUrl = 'https://github.com/shogunsea/react-2048/compare/';
 
   readPreviousSavedSHA()
   .then(fetchCurrentSHA)
-  .then(updateSavedSHA)
   .then(({previousSHA, currentSHA}) => {
     diffUrl += previousSHA + '...' + currentSHA;
     console.log(
@@ -82,16 +83,23 @@ gulp.task('deploy', 'Deploy to production host', () => {
       chalk.yellow(currentSHA) +
       chalk.blue(' to production host...')
     );
-    return {currentSHA, previousSHA};
+    return {currentSHA, previousSHA, deployCommand};
   }).then(deployToProduction)
   .then(({previousSHA, currentSHA}) => {
     if (previousSHA === currentSHA) {
       console.log(
-        chalk.yellow('Deployment complete successfully, no chnages observed.')
+        chalk.yellow('Deployment completed successfully, no chnages observed.')
       );
     } else {
-      console.log(chalk.green('Deployment complete successfully, changes: '));
+      console.log(chalk.green('Deployment completed successfully, changes: '));
       console.log(chalk.underline(diffUrl));
     }
+    return {previousSHA, currentSHA};
+  }).then(updateSavedSHA)
+  .catch((err) => {
+    console.log(
+      chalk.red('Deployment failed, local SHA file didnt get updated. Error: ')
+    );
+    console.log(err);
   });
 });
