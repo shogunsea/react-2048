bb74b0731df30be086b20e7768461586121f323e
shogunsea <shogunsea08@gmail.com>
2017-06-02 09:48:21 -0500

    potential test cases placeholder

--- test/helper/index.test.js
+++ test/helper/index.test.js
@@ -25,8 +25,30 @@ describe('Board helper file', () => {
         const output = rotateMatrixClockwise(this.input);
         expect(this.expectedOutput).to.deep.equal(output);
       });
-    })
+    });
+
+  //   context('when the matrix is a 3*4 rectangle', () => {
+  //     beforeEach(() => {
+  //       this.input = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
+  //       this.expectedOutput = [[9,5,1],[10,6, 2],[11,7,3],[12,8,4]];
+  //     });
+
+  //     it('rotates the matrix correctly', () => {
+  //       const output = rotateMatrixClockwise(this.input);
+  //       expect(this.expectedOutput).to.deep.equal(output);
+  //     });
+  //   });
+
+  //   context('when the matrix has different row size', () => {
+  //     beforeEach(() => {
+  //       this.input = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
+  //       this.expectedOutput = [[9,5,1],[10,6, 2],[11,7,3],[12,8,4]];
+  //     });
+
+  //     it('rotates the matrix correctly', () => {
+  //       const output = rotateMatrixClockwise(this.input);
+  //       expect(this.expectedOutput).to.deep.equal(output);
+  //     });
+  //   });
   })
 });
-
-
